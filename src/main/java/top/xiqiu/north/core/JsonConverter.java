f5dbecc7c882c7fbe1ab4d9bf55cf8aaa9923eea
wangxian <xian366@126.com>
2022-08-12 20:38:42 +0800

    新增：JsonConverter

---
+++ src/main/java/top/xiqiu/north/core/JsonConverter.java
@@ -0,0 +1,54 @@
+package top.xiqiu.north.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+/**
+ * json 转换类，方便更换类库
+ */
+public class JsonConverter {
+    private Gson gson;
+
+    public JsonConverter() {
+        gson = new GsonBuilder()
+                // 序列化null
+                .serializeNulls()
+                // 设置日期时间格式，另有2个重载方法
+                // 在序列化和反序化时均生效
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                // 禁此序列化内部类
+                .disableInnerClassSerialization()
+                // 禁止转义html标签
+                .disableHtmlEscaping()
+                // 格式化输出
+                // .setPrettyPrinting()
+                .create();
+    }
+
+    /**
+     * 转换为字符串
+     */
+    public String stringify(Object obj) {
+        return gson.toJson(obj);
+    }
+
+    /**
+     * 字符串解析为 Java bean
+     */
+    public <T> T parse(String jsonStr, Class<T> jsonType) {
+        return gson.fromJson(jsonStr, jsonType);
+    }
+
+    /**
+     * 字符串解析为 Map/List（泛型解析，注意类型擦除）
+     */
+    public <T> T parse(String jsonStr) {
+        Type type = new TypeToken<T>() {
+        }.getType();
+        return gson.fromJson(jsonStr, new TypeToken<T>() {
+        }.getType());
+    }
+}
--- src/test/java/top/xiqiu/AppTest.java
+++ src/test/java/top/xiqiu/AppTest.java
@@ -1,7 +1,17 @@
 package top.xiqiu;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Assert;
 import org.junit.Test;
+import top.xiqiu.north.core.JsonConverter;
+import top.xiqiu.test.entity.Login;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -13,5 +23,79 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testJsonConverter2String() {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("h", "<body><br /><p id=\"name\"></p></body>");
+        data.put("name", "wangxian");
+        data.put("now", new Date());
+
+        String jsonString = new JsonConverter().stringify(data);
+        System.out.println(jsonString);
+    }
+
+    /**
+     * gson 方法
+     */
+    @Test
+    public void testJsonConverter2Json1() {
+        String jsonString = "{\"now\":\"2022-08-12 17:43:50\",\"h\":\"<body><br /><p id=\\\"name\\\"></p></body>\",\"name\":\"wangxian\"}";
+        final Type type = new TypeToken<HashMap<String, Object>>() {
+        }.getType();
+        final Map<String, Object> parse = new Gson().newBuilder().create().fromJson(jsonString, type);
+        System.out.println(parse.get("now"));
+    }
+
+    /**
+     * JsonConverter 封装 - bean 类
+     */
+    @Test
+    public void testJsonConverter2Json2() {
+        String jsonString = "{\"email\":\"wangxian@wboll.com\"}";
+        Login login = new JsonConverter().parse(jsonString, Login.class);
+
+        System.out.println("email=" + login.email);
+    }
+
+    /**
+     * JsonConverter 封装 - 泛型
+     */
+    @Test
+    public void testJsonConverter2Json3() {
+        // 解析为 map
+        String jsonString = "{\"now\":\"2022-08-12 17:43:50\", \"h\":\"<body><br /><p id=\\\"name\\\"></p></body>\"}";
+        final Map<String, Object> userMap = new JsonConverter().<Map<String, Object>>parse(jsonString);
+        System.out.println(userMap.get("h"));
+
+        // 解析为 list
+        jsonString = "[\"x@x.com\", \"b@b.com\"]";
+        List<String> usersList = new JsonConverter().parse(jsonString);
+        System.out.println("usersList.get(0)=" + usersList.get(0));
+
+        // 解析为 array
+        jsonString = "[{\"email\": \"x@x.com\"}, {\"email\": \"b@b.com\"}]";
+        Login[] userArray = new JsonConverter().parse(jsonString, Login[].class);
+        System.out.println("userArray[0].email=" + userArray[0].email);
+
+        // // 解析为 List，存在泛型类型擦除丢失的情况，以下在运行时报错
+        // jsonString = "[{\"email\": \"x@x.com\"}, {\"email\": \"b@b.com\"}]";
+        // List<Login> listLogin = new JsonConverter().<List<Login>>parse(jsonString);
+        // String email = listLogin.get(0).email;
+        // System.out.println("listLogin.email=" + email);
+
+        // 下面的 Map<String, String> 也被擦除了
+        // jsonString = "[{\"email\": \"x@x.com\"}, {\"email\": \"b@b.com\"}]";
+        // List<Map<String, String>> listLogin = new JsonConverter().parse(jsonString);
+        // String email = listLogin.get(1).get("email");
+        // System.out.println("listLogin.email=" + email);
+
+        // 使用 gson 可以执行
+        Gson gson = new Gson();
+        List<Login> users = gson.fromJson(jsonString, new TypeToken<List<Login>>() {
+        }.getType());
+        String email2 = users.get(0).email;
+        System.out.println("gson.email=" + email2);
     }
 }
