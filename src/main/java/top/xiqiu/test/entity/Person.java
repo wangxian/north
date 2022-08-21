f7c5ecb0e3494e6bd3d177a60fe91edb8786ff71
wangxian <xian366@126.com>
2022-08-22 02:08:37 +0800

    修改：测试非spring下集成 mybatis-plus，以及 p6spy

---
+++ src/main/java/top/xiqiu/test/entity/Person.java
@@ -0,0 +1,37 @@
+package top.xiqiu.test.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("person")
+public class Person {
+    private Long id;
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
